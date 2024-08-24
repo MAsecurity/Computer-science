a4ace3675c4701e2d5a6d6a11d0099e6d4053152
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-08-24 04:14:42 +0100

    Finish recursive Fibonacci

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -1,3 +1,4 @@
+// iterative solution
 function fibs(n){
  let array = [0,1];
  for(let i=0; i<n-2; i++){
@@ -7,3 +8,15 @@ function fibs(n){
  return array;
 }
 console.log(fibs(8));
+
+// recursive solution
+function fibsRec(n){
+  if(n == 2){
+    return [0,1];
+  }else{
+    let result = fibsRec(n-1);
+    let sum = result[result.length - 2] + result[result.length - 1];
+    return result.concat([sum]) 
+  }
+}
+console.log(fibsRec(8));
