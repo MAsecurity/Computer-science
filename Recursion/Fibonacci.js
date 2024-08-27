ec9ab84b7ddeab115341bd9a13e09b5575bd4ab8
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-08-27 02:02:24 +0100

    Update

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -1,23 +1,31 @@
 // iterative solution
 function fibs(n){
- let array = [0,1];
+ let array = [0];
  for(let i=0; i<n-1; i++){
-  let sum = array[array.length - 2] + array[array.length - 1];
-  array.push(sum);
+  if(array.length < 2){
+    array.push(1);
+  }else{
+    let sum = array[array.length - 2] + array[array.length - 1];
+    array.push(sum);
+  }
  }
  return array;
 }
-console.log(fibs(8)); // 0,1,1,2,3,5,8,13,21
+console.log(fibs(8)); // 0,1,1,2,3,5,8,13
 
 // recursive solution
 function fibsRec(n){
-  if(n <= 1){
-    return [0,1];
+  if(n <= 0){
+    return [0];
   }else{
     let result = fibsRec(n-1);
-    let sum = result[result.length - 2] + result[result.length - 1];
-    return result.concat([sum])
+    if(result.length < 2){
+      return result.concat([1]);
+    }else{
+      let sum = result[result.length - 2] + result[result.length - 1];
+      return result.concat([sum])
+    }
   }
 }
 let num = 8;
-console.log(`This was done recursively: ${fibsRec(num)}`); // 0,1,1,2,3,5,8,13,21
+console.log(`This was done recursively: ${fibsRec(num-1)}`); // 0,1,1,2,3,5,8,13
