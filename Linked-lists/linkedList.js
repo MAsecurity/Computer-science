e7eed122bceb2a1042f60e91e6dfe21063182d1b
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-09-01 06:07:35 +0100

    Finish at, tail and updated toString

--- Linked-lists/linkedList.js
+++ Linked-lists/linkedList.js
@@ -31,14 +31,28 @@ class linkedList{
     return i;
 
   }
-  head(){
+  headMethod(){
     return this.head.value;
 
   }
   tail(){
-    
+    let tmp = this.head;
+    while(tmp.nextNode != null){
+      tmp = tmp.nextNode;
+    }
+    return tmp.value;
   }
   at(index){
+    let tmp = this.head;
+    let curI = -1;  // we want to start incrementing with 0 being the first incrementation;
+    while(tmp.nextNode != null){
+      curI++;
+      if(curI == index){
+        return tmp.value;
+      }
+      tmp = tmp.nextNode;
+    }
+    return `Sorry we dont have an element at that given index: ${index}`;
 
   }
   pop(){
@@ -51,13 +65,32 @@ class linkedList{
 
   }
   toString(){
-    return this.head;
+    let string = [];
+    let tmp = this.head;
+    while(tmp){
+      if(tmp.nextNode == null){
+        string.push(`(${tmp.value}) -> null`);
+        break;
+      }else{
+        string.push(`(${tmp.value}) -> `);
+        tmp = tmp.nextNode;
+      }
+    }
+    return string.join("");
   }
+
 
 }
 let list = new linkedList();
 list.append("dog");
-list.append("cat")
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 list.prepend("dragon");
 console.log(list.size())
+console.log(list.headMethod())
+console.log(list.tail());
+console.log(list.at(2));
 console.log(list.toString());
