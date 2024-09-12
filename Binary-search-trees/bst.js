622e772f2800039baca4572b32a219c7abe1761f
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-09-12 01:37:07 +0100

    Add find method

--- Binary-search-trees/bst.js
+++ Binary-search-trees/bst.js
@@ -18,6 +18,11 @@ class Tree{
     return root;
   }
   insert(value){
+    // check for duplicates first
+    let dupCheck = this.find(value);
+    if(dupCheck){
+      return 'Cannot insert duplicates';
+    }
     let tmp = this.root;
     while(tmp.left || tmp.right){
       if(tmp.data > value){
@@ -32,6 +37,25 @@ class Tree{
     }else{
       tmp.right = newNode;
     }
+  }
+  deleteItem(value){
+    
+  }
+  find(value){
+    let tmp = this.root;
+    while(tmp.left || tmp.right){
+      if(tmp.data == value){
+        return tmp;
+      }if(tmp.data > value){
+        tmp = tmp.left;
+      }else{
+        tmp = tmp.right;
+      }
+    }
+    if(tmp.data == value ){
+      return tmp;
+    }
+    return false;
   }
 }
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -70,3 +94,4 @@ let tree = new Tree();
 console.log(tree.buildTree(arr))
 console.log(tree.insert(2));
 console.log(prettyPrint(tree.root))
+console.log(tree.find(92))
