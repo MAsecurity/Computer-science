8698ba8d1f390bfed8ee18677826a38850f81133
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-11-11 03:56:26 +0000

    Add psuedocode

---
+++ Knights-travails/knightsTravails.js
@@ -0,0 +1,23 @@
+// build the adjacency list
+// find possible moves and ensure they are within the board and have not been visited
+// update map
+// dfs and return shortest path
+class knightTravails {
+  constructor(position,target){
+    this.position = position;
+    this.target = target;
+    this.map = new Map();
+    this.set = new Set()
+  }
+  buildList(){
+
+  }
+  isValid(){
+
+  }
+  possibleMoves(){
+
+  }
+
+
+}
